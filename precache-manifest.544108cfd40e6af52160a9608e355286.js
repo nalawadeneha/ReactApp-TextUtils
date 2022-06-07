84e2135c94e4ad225263582129ccd10322526583
nalawadeneha <nalawadeneha91@gmail.com>
2022-06-07 20:01:23 +0530

    Updates

---
+++ precache-manifest.544108cfd40e6af52160a9608e355286.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b74a83bb648feaf7969e",
+    "url": "/my-app/static/css/main.b2e526ac.chunk.css"
+  },
+  {
+    "revision": "b74a83bb648feaf7969e",
+    "url": "/my-app/static/js/main.51045eb6.chunk.js"
+  },
+  {
+    "revision": "cfa7322796517912f0d1",
+    "url": "/my-app/static/js/runtime~main.7a725f0a.js"
+  },
+  {
+    "revision": "34928b59d328e5af44cd",
+    "url": "/my-app/static/js/2.7d51d47b.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/my-app/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "8c57ef154cacd3a0c518dbdd101bd7b4",
+    "url": "/my-app/index.html"
+  }
+];
